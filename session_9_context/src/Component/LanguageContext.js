db2ca5af113d6fc00a41b64e69e7e2a82e55e863
sonvh0405 <sonvh0405@gmail.com>
2022-04-13 21:22:48 +0700

    languagecontext

--- session_9_context/src/Component/LanguageContext.js
+++ session_9_context/src/Component/LanguageContext.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react'
 
-const LanguageContext = createContext({language: "vi",});
+const LanguageContext = createContext({ language: "vi", });
 
 export default LanguageContext;
